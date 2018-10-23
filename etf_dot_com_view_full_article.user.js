f0a184322b76ad2dcc29ff64b189dc1c3a9b5e33
Jacob Egner <jmegner@gmail.com>
2018-10-23 10:20:48 -0500

    v0.4; add/modify metadata tags for github stuff

--- etf_dot_com_view_full_article.user.js
+++ etf_dot_com_view_full_article.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name        etf.com view full article
-// @namespace   JacobEgner
-// @version     0.3
+// @namespace   https://github.com/jmegner
+// @version     0.4
 // @description Redirects to full article view for articles at etf.com.
-// @license     CC0-1.0
+// @license     Unlicense
+// @homepageURL https://github.com/jmegner/greasemonkey_etf_dot_com_view_full_article
+// @supportURL  https://github.com/jmegner/greasemonkey_etf_dot_com_view_full_article/issues
 // @match       https://www.etf.com/sections/*
 // @run-at      document-start
 // ==/UserScript==
 
 // Only redirect if we're the top window.
-// This condition prevents iframes embedded within etf.com pages from 
-// triggering redirects themselves: we only want the outer window to do that. 
+// This condition prevents iframes embedded within etf.com pages from
+// triggering redirects themselves: we only want the outer window to do that.
 // It has the side-effect that if etf.com is embedded
 // in a frame on some other website, we'll skip doing the redirect
 
